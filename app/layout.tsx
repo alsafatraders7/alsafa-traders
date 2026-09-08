608171d7cb52da12776c3397b6d080398dc545f6
alsafatraders7 <alsafatraders7@gmail.com>
2026-09-09 00:09:58 +0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,4 @@
+export const metadata = { title: 'Al Safa Traders.pk' }
+export default function RootLayout({children}:{children:React.ReactNode}){
+  return <html lang="en"><body style={{margin:0}}>{children}</body></html>
+}
