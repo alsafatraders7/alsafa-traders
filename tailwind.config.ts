e945ecc9fb51f4df4efb47c6c4d584120235c6ea
alsafatraders7 <alsafatraders7@gmail.com>
2026-09-11 04:48:51 +0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,6 @@
-const config = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+import type { Config } from "tailwindcss";
+const config: Config = {
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: { extend: {} },
   plugins: [],
 };
