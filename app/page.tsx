4ecd98bfbe43d10b7112df03ac0832d491dfd2cd
alsafatraders7 <alsafatraders7@gmail.com>
2026-09-11 04:27:48 +0400

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-[#070707] text-white flex flex-col items-center justify-center">
-      <h1 className="text-5xl font-black">AL <span style={{color:'#D4AF37'}}>SAFA</span> TRADERS</h1>
-      <p className="mt-3 text-gray-400">Website Live Ho Gayi!</p>
-    </div>
-  )
-}
