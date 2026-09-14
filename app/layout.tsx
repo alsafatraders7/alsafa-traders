529727979460d98312897a0c62e673428cd7bfe1
alsafatraders7 <alsafatraders7@gmail.com>
2026-09-14 04:52:35 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,12 @@
 import "./globals.css";
+import Tawk from "./components/Tawk";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         {children}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/68ba7c43af3730192aed0d68/1j66g4j1g';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-              })();
-            `,
-          }}
-        />
+        <Tawk />
       </body>
     </html>
   );
