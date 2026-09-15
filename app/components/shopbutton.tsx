d0936956c185b705e7d2464a603e411a3f0d73ff
alsafatraders7 <alsafatraders7@gmail.com>
2026-09-15 10:15:36 +0400

    Update shopbutton.tsx

--- app/components/shopbutton.tsx
+++ app/components/shopbutton.tsx
@@ -1,10 +1,2 @@
 "use client"
-import { getShopLink, getShopText } from "@/lib/shop"
-
-export default function ShopButton({ link }: { link: string }) {
-  return (
-    <a href={getShopLink(link)} target="_blank" rel="noopener noreferrer" onClick={(e) => e.stopPropagation()} className="block text-center bg-[#FFD814] hover:bg-[#F7CA00] py-2.5 rounded-full text-[12px] font-black text-black">
-      {getShopText()}
-    </a>
-  )
-}
+import { getShopLink, getShopText } from '../../lib/shop'
